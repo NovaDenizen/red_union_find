ebc82644110525b0b01ebce13126ff08e4a8df67
Ken Bateman <novadenizen@gmail.com>
2020-11-07 14:01:29 -0500

    comments, added as_permutation

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,13 @@ use num;
 use std::cell::Cell;
 
 #[derive(Clone, Debug)]
+/// Implements union-find with internal mutability
+///
+/// The `find` method 
 struct UF<I: Copy> {
     /// invariants: 
     ///     1.  leaders[i] <= i, so whenever unioning two indices, the bigger will point to the
-    ///            smaller
-    ///     2.  all leaders[i] >= 0 && <= leaders.len()
+    ///            smaller.  This prevents (non-identity) cycles.
     leaders: Vec<Cell<I>>,
 }
 
@@ -24,7 +26,7 @@ where
                 "Tried to compare equality on two UF with different sizes");
         for i_idx in 0..self.leaders.len() {
             let i = I::from_usize(i_idx).unwrap();
-            if self.const_find(i) != other.const_find(i) {
+            if self.find(i) != other.find(i) {
                 return false;
             }
         }
@@ -50,8 +52,11 @@ where
         }
         UF { leaders }
     }
-    pub fn len(&self) -> I {
+    pub fn max(&self) -> I {
         I::from_usize(self.leaders.len()).unwrap()
+    }
+    pub fn len(&self) -> usize {
+        self.leaders.len()
     }
     fn const_find(&self, mut i: I) -> I {
         loop {
@@ -62,7 +67,12 @@ where
             i = l;
         }
     }
-    pub fn find(&mut self, i: I) -> I {
+    /// Returns representative with minimum index from equivalence class.
+    ///
+    /// This method purports to immutably use `&self`, but it really uses interior mutability to
+    /// implement path-compression in the safe, traditional way.  Almost every other method on `UF`
+    /// delegates to `find`, so they are all lying too.
+    pub fn find(&self, i: I) -> I {
         let cell = &self.leaders[i.into()];
         let l = cell.get();
         // TODO: optimize for common case
@@ -92,6 +102,11 @@ where
         }
     }
 
+    /// Perform union
+    ///
+    /// Since this structure uses interior mutability, the `mut` attribute is not strictly
+    /// necessary.  But it communicates to the user that the data structure is logically changing,
+    /// so it's a worthwhiile part of the API.
     pub fn union(&mut self, i: I, j: I) {
         // The mutability here is really not necessary, but I think it is effective at
         // astonishment reduction.
@@ -103,11 +118,10 @@ where
             self.leaders[l_i.into()].set(l_j);
         }
     }
-    pub fn same_set(&mut self, i: I, j: I) -> bool {
+    pub fn same_set(&self, i: I, j: I) -> bool {
         self.find(i) == self.find(j)
     }
-
-    pub fn equivalence_union(a: &mut Self, b: &mut Self) -> Self {
+    pub fn equivalence_union(a: &Self, b: &Self) -> Self {
         assert!(a.leaders.len() == b.leaders.len(), "Called equivalence_union on two UF of different sizes");
         let mut res = a.clone();
         for idx in 0..b.leaders.len() {
@@ -116,7 +130,7 @@ where
         }
         res
     }
-    pub fn equivalence_intersection(a: &mut Self, b: &mut Self) -> Self {
+    pub fn equivalence_intersection(a: &Self, b: &Self) -> Self {
         // TODO: discover a better than O(n^2) algorithm for this.
         assert!(a.leaders.len() == b.leaders.len(), "Called equivalence_union on two UF of different sizes");
         let len = a.leaders.len();
@@ -133,10 +147,38 @@ where
         }
         res
     }
+
+    /// Ensure all leader paths are minimal
+    fn canonicalize(&self) {
+        for idx in 0..self.leaders.len() {
+            let i = I::from_usize(idx).unwrap();
+            self.find(i);
+        }
+    }
+    /// Creates permutation version of a UF
+    ///
+    /// each equivalence group corresponds to a cycle.  Every link in a cycle points downward,
+    /// except for the smallest which points at the largest element in the cycle.
+    fn as_permutation(&self) -> Vec<I> {
+        let mut res = Vec::with_capacity(self.leaders.len().into());
+        for idx in 0..self.len() {
+            let i = I::from_usize(idx).unwrap();
+            let j = self.find(i);
+            if i == j {
+                res.push(j); // identity cycle
+            } else { // points to the leader
+                res.push(res[j.into()]); // This cell points back at the previous max
+                res[j.into()] = i;  // leader now points at this one, the new max
+            }
+        }
+        res
+    }
     #[cfg(test)]
     /// Ensures all expected invariants hold
     ///
     /// The only one I can think of is that leaders[i] <= i
+    ///
+    /// This is *actually* const.
     fn assert_invariants(&self) 
     where
         I: std::fmt::Display,
@@ -165,6 +207,8 @@ where
     ///
     /// Not actually unsafe, but requires understanding of internals to use properly, so it's
     /// marked as such.
+    ///
+    /// This is **actually** const
     unsafe fn struct_eq(&self, other: &Self) -> bool {
         self.leaders == other.leaders
     }
@@ -192,10 +236,10 @@ mod tests {
         println!("residue_class({},{}) = {:?}", len, m, res);
         res
     }
-    fn assert_is_residue_class(m: T, a: &mut UF<T>) {
+    fn assert_is_residue_class(m: T, a: &UF<T>) {
         println!("checking if UF is residue_class {}", m);
-        for i in 0..a.len() {
-            for j in 0..a.len() {
+        for i in 0..a.max() {
+            for j in 0..a.max() {
                 let same_res = (i % m) == (j % m);
                 let same_set = a.same_set(i,j);
                 assert_eq!(same_res, same_set, "m=={} i=={} j=={} same_set=={}", m, i, j, same_set);
@@ -204,12 +248,12 @@ mod tests {
     }
     #[test]
     fn try_2_3_6() {
-        let mut x2 = residue_class(100, 2);
-        assert_is_residue_class(2, &mut x2);
-        let mut x3 = residue_class(100, 3);
-        assert_is_residue_class(3, &mut x3);
-        let mut y6 = UF::equivalence_intersection(&mut x2, &mut x3);
-        assert_is_residue_class(6, &mut y6);
+        let x2 = residue_class(100, 2);
+        assert_is_residue_class(2, &x2);
+        let x3 = residue_class(100, 3);
+        assert_is_residue_class(3, &x3);
+        let y6 = UF::equivalence_intersection(&x2, &x3);
+        assert_is_residue_class(6, &y6);
     }
 
 
@@ -224,6 +268,17 @@ mod tests {
             let u = UF::from_slice(&U_VALS[..]);
             t.assert_invariants();
             assert!(t.struct_eq(&u));
+        }
+    }
+    #[test]
+    fn permutation_test() {
+        unsafe {
+            const T_VALS: [u16; 10] = [ 0, 0, 2, 3, 2, 5, 6, 7, 7, 7 ];
+            let t = UF::from_slice(&T_VALS[..]);
+            t.assert_invariants();
+            const U_VALS: [u16; 10] = [ 1, 0, 4, 3, 2, 5, 6, 9, 7, 8 ];
+            let u = t.as_permutation();
+            assert_eq!(&U_VALS[..], &u[..]);
         }
     }
 }
